a5d1ee0671a54c6ca9e54a031ee6aa4cf8ab82c0
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-06 17:49:44 +0100

    show menu

--- Desktop/beginner course/day-2/day-2-ruby-basics/pizzeria.rb
+++ Desktop/beginner course/day-2/day-2-ruby-basics/pizzeria.rb
@@ -9,3 +9,10 @@ def customer_name
   print "What's your name? "
   return gets.chomp
 end
+
+def show_menu
+  puts "1 - Salami - #{@pizza_salami} Euro"
+  puts "2 - Quattro Stagioni - #{@pizza_quattro_stagioni} Euro"
+  puts "3 - Ham / Cheese - #{@pizza_ham_cheese} Euro"
+  puts "4 - Shoarma - #{@pizza_shoarma} Euro"
+end
